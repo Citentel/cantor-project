2f83d78b5cf7f11413dcd81ab7fa1cbc4d4b1d53
Citentel <gontrebor@gmail.com>
2020-10-10 21:17:27 +0200

    add default style for app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import "./static/css/app.css";
 
 class App extends Component {
     render = () => {
         return(
             <div className="app">
-                <h1>Kalkulator walutowy</h1>
                 <div className="app__box">
+                    <h1 className="app__box--h1">Kalkulator walutowy</h1>
                     test
                 </div>
             </div>
