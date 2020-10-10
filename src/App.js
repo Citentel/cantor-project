b4174a546f3ef127622ee5196a6a420871775592
Citentel <gontrebor@gmail.com>
2020-10-10 21:28:59 +0200

    create NBP api connection

--- src/App.js
+++ src/App.js
@@ -2,12 +2,45 @@ import React, { Component } from 'react';
 import "./static/css/app.css";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url : 'http://api.nbp.pl/api/exchangerates/tables/a',
+            isLoading : true,
+            json : []
+        }
+        
+        this.getData();
+    } 
+
+    getConnect = async () => {
+        let response = await fetch(this.state.url);
+        if (!response.ok) {
+            console.log(response.status);
+            return false;
+        }
+        else {
+            return response;
+        }
+    }
+
+    getData = async () => {
+        if (this.getConnect() !== false) {
+            const json = await this.getConnect().then((response)=> response.json());
+
+            this.setState({
+                json : json,
+                isLoading : false
+            });
+        }
+    }
+
     render = () => {
         return(
             <div className="app">
                 <div className="app__box">
                     <h1 className="app__box--h1">Kalkulator walutowy</h1>
-                    test
+                    form
                 </div>
             </div>
         );
